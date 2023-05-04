bd62f9e9e0aa4c366a54455300c88b893eb60136
envelope <chaselonas@gmail.com>
2023-05-03 17:20:44 -0700

    initial implementation of drop down details for income card

--- frontend/src/components/dashboard.rs
+++ frontend/src/components/dashboard.rs
@@ -1,8 +1,7 @@
 use yew::prelude::*;
-use yew::function_component;
 use wasm_bindgen_futures::spawn_local;
 use reqwest::Client;
-
+use log::info;
 
 use shared::models::income::Income;
 
@@ -11,17 +10,26 @@ pub struct Props {
     pub selected_user_id: Option<u32>,
 }
 
-// #[derive(Default)]
-// pub struct DashboardState {
-//     total_income: u32,
-// }
-
 #[function_component(Dashboard)]
 pub fn dashboard(props: &Props) -> Html {
     let total_income = use_state(|| 0_u32);
     let selected_user_id = props.selected_user_id;
+    let is_expanded = use_state(|| false);
+
+
+    let toggle_expanded = {
+    let is_expanded = is_expanded.clone();
+        Callback::from(move |_| {
+            is_expanded.set(!*is_expanded);
+            // info!("Toggle Income Details clicked, is_expanded: {}", *is_expanded);
+        })
+    };
+
+
+    let income_sources = use_state(|| Vec::<Income>::new());
 
     let total_income_clone = total_income.clone();
+    let income_sources_clone = income_sources.clone();
 
     use_effect_with_deps(
         move |_| {
@@ -38,6 +46,7 @@ pub fn dashboard(props: &Props) -> Html {
                                 if let Ok(incomes) = resp.json::<Vec<Income>>().await {
                                     let total: u32 = incomes.iter().map(|income| income.amount).sum();
                                     total_income_clone.set(total);
+                                    income_sources_clone.set(incomes);
                                 }
                             } else {
                                 log::warn!("Failed to fetch income data");
@@ -64,7 +73,33 @@ pub fn dashboard(props: &Props) -> Html {
         <section class="dashboard">
             <div class="dashboard-card">
                 <h3>{ "Total Income" }</h3>
-                <p>{ format!("User's total income: {:?}", *display_total_income) }</p>
+                <p>{ format!("User's total income: {}", *display_total_income) }</p>
+                <button onclick={toggle_expanded.clone()}>{ "Toggle Income Details" }</button>
+                {
+                    if *is_expanded {
+                        let display_income_sources = income_sources.clone();
+                        html! {
+                            <ul>
+                                { for display_income_sources.iter().map(|income| {
+                                    let onmouseover = Callback::from({
+                                        let income_description = income.description.clone();
+                                        move |_| {
+                                            info!("{:?}", income_description.as_deref().unwrap_or(""));
+                                        }
+                                    });
+                                    html! {
+                                        <li onmouseover={onmouseover}>
+                                            <span>{ &income.name }</span>
+                                            <span>{ format!("${}", income.amount) }</span>
+                                        </li>
+                                    }
+                                })}
+                            </ul>
+                        }
+                    } else {
+                        html! {}
+                    }
+                }
             </div>
             <div class="dashboard-card">
                 <h3>{ "Total Expenses" }</h3>
